8bd986e5e6b7f4bb3cd43b67caeef0c6040f7f6a
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-09-16 21:46:59 -0600

    checking if vercel bot is integrated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main>
       <h1 className='text-3xl font-bold text-indigo-500'>Discord Clone</h1>
       <Button className='bg-indigo-500'>Button</Button>
+      <p>checking if vercel bot is integrated</p>
     </main>
   )
 }
