45ca9a6bc728a3f6192c0ec2808e8131d88b2890
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-09-17 22:13:26 -0600

    added and configure prisma with schemas

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -4,6 +4,7 @@ import { UserButton } from '@clerk/nextjs'
 export default function Home() {
   return (
     <main>
+      <p className='text-indigo-500 text-3xl font-bold'>Discord Clone</p>
       <UserButton
         afterSignOutUrl='/'
       />
